f7df2c014dd46b075afd5d5bddbb59f51fe71d43
My GIT <saff2912@gmail.com>
2019-09-13 23:04:02 +0800

    Laba 1.3 Проверка пароля

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "zxcvbnm";
+            string str2;
+
+            Console.Write("Введите пароль: ");
+            str2 = Console.ReadLine();
+
+            int result = String.Compare(str1, str2);
+            if (result!=0)
+            {
+                Console.WriteLine("Строки отличаются");
+            }
+            else
+            {
+                Console.WriteLine("Строки идентичны");
+            }
+            
+        }
+    }
+}
